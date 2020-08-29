bf13bd0b8b033c361df6b189119975a2971e0939
Muhammad Faraz <muhammadfaraz991@gmail.com>
2020-08-29 20:25:45 +0500

    Increament and Decrement Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [num, setnum] = useState(0);
+
+  const Increament = (event) => {
+    setnum(num+1);
+  }
+  const Decreament = (event) => {
+    if(num>0){
+      setnum(num-1)
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>{num}</h1>
+          <div className="btn_div">
+            <button onClick={Increament}>Inc</button>
+            <button onClick={Decreament}>Dec</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
